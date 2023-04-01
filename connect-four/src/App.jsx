07c9e7a61302e5215981a7c17af1c991f03e56f3
nebjamin <jonb1@newpaltz.edu>
2023-03-31 23:54:41 -0400

    Initialize React Project

---
+++ connect-four/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect} from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
